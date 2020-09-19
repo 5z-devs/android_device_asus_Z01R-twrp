238b127041b3915667c8cb1b1fe43ad65e3f5a6c
Marc Bourgoin <themard85@gmail.com>
2020-09-18 18:52:19 -0600

    Z01R: Switch to FBE
    Change-Id: If02f4b0fd43f3d6414bf697d9d0496a932428b26

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,7 +111,6 @@ TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_CRYPTO_FBE := true
 PLATFORM_VERSION := 16.1.0
 PLATFORM_SECURITY_PATCH := 2099-12-31
-TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
